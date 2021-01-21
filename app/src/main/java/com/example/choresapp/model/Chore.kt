f9cee8bcc70376bd975bf58d91e92547a0fe7a88
shaipova <68189925+shaipova@users.noreply.github.com>
2021-01-21 13:53:14 +0300

    update class Chore

--- app/src/main/java/com/example/choresapp/model/Chore.kt
+++ app/src/main/java/com/example/choresapp/model/Chore.kt
@@ -1,22 +1,8 @@
 package com.example.choresapp.model
 
-import java.text.DateFormat
-import java.util.*
-
-class Chore() {
-    var choreName: String? = null
-    var assignedBy: String? = null
-    var assignedTo: String? = null
-    var id: Int? = null
-
-    constructor(choreName: String,
-                assignedBy: String,
-                assignedTo: String,
-                id: Int): this() {
-
-        this.choreName = choreName
-        this.assignedBy = assignedBy
-        this.assignedTo = assignedTo
-        this.id = id
-    }
-}
+class Chore(
+    var choreName: String = "null",
+    var assignedBy: String = "null",
+    var assignedTo: String = "null",
+    var id: Int? = 0
+)
